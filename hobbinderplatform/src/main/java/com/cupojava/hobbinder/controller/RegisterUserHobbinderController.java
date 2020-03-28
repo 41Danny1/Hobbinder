42021936730e11c572e15af5ea61d008db9e49ad
julianatakase2904 <takasej@student.douglascollege.ca>
2020-03-28 16:59:07 -0700

    Update RegisterUserHobbinderController.java

--- hobbinderplatform/src/main/java/com/cupojava/hobbinder/controller/RegisterUserHobbinderController.java
+++ hobbinderplatform/src/main/java/com/cupojava/hobbinder/controller/RegisterUserHobbinderController.java
@@ -1,1 +1,38 @@
+package com.cupojava.hobbinder.controller;
+
+import java.time.LocalDateTime; 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.cupojava.hobbinder.model.HelloWorld; 
+import com.cupojava.hobbinder.model.Message;
+import com.cupojava.hobbinder.model.User;
+
+@Controller
+public class RegisterUserHobbinderController {
+
+	
+	@RequestMapping("/registerUser")
+	public String handler(Model model) {
+
+		HelloWorld helloWorld = new HelloWorld();
+		List<Message> strings = new ArrayList<Message>();
+		for(int i = 0; i < 5; i++) {
+			String tmpStr = "String" + i;
+			Message msg = new Message();
+			msg.setContent(tmpStr);
+			strings.add(msg);
+		}
+		
+		helloWorld.setMessages(strings);
+		helloWorld.setDateTime(LocalDateTime.now().toString());
+		model.addAttribute("temp", helloWorld);
+		return "registerUser";
+	}
+}
 
