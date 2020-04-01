022e31d6d71596059fb52f875da8f16cbfe313f9
41danny1 <danny@sdgcore.com>
2020-03-31 17:34:30 -0700

    Adding code to render relevant posts according to community ID in GET request

--- hobbinderplatform/src/main/java/com/cupojava/hobbinder/controller/CommunityController.java
+++ hobbinderplatform/src/main/java/com/cupojava/hobbinder/controller/CommunityController.java
@@ -1,14 +1,13 @@
 package com.cupojava.hobbinder.controller;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.cupojava.hobbinder.model.Header;
 import com.cupojava.hobbinder.model.Post;
@@ -20,17 +19,32 @@ public class CommunityController {
 	@Autowired
 	PostDao postDao;
 
-	@RequestMapping("/community")
+	@RequestMapping(value = "/community", params = "id")
+	public String handler(int id, Model model) {
+		Header header = new Header();
+		model.addAttribute("headerTemplate", header);
+
+		Post p = postDao.findPostByCode(1);
+
+		String posts = "";
+		List<Post> postObjects = postDao.findPostsByCommunity(id);
+		for(int i=0; i<postObjects.size(); i++)
+			posts += postObjects.get(i).render();
+		
+		model.addAttribute("posts", posts);
+
+		return "communityLayout";
+	}
+	
+	@RequestMapping(value = "/community") //EDIT LATER TO RETURN AN ERROR PAGE FOR EMPTY COMMUNITY!
 	public String handler(Model model) {
 		Header header = new Header();
 		model.addAttribute("headerTemplate", header);
 
 		Post p = postDao.findPostByCode(1);
-//		System.out.println(p.render());
-//		System.out.println(postDao.findPostsByCommunity(1).size());
-//		model.addAttribute("posts", p.render());
+
 		String posts = "";
-		List<Post> postObjects = postDao.findPostsByCommunity(1);
+		List<Post> postObjects = postDao.findPostsByCommunity(0);
 		for(int i=0; i<postObjects.size(); i++)
 			posts += postObjects.get(i).render();
 		
